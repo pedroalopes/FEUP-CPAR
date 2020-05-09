cd6fcd5462b9a98953dbf59436ea627df4ffea58
vieirajlt <up201603190@fe.up.pt>
2020-05-09 13:21:29 +0100

    Clock bug fix and overall optimisation on sequential

--- Project 2/sieve_seq.cpp
+++ Project 2/sieve_seq.cpp
@@ -6,9 +6,11 @@
 
 using namespace std;
 
+typedef long long ll;
+
 int main (int argc, char *argv[])
 {
-    long long n;
+    ll n;
     
     if(argc >= 2) {
         n = 2<<(atoi(argv[1])-1);
@@ -19,11 +21,11 @@ int main (int argc, char *argv[])
         n = pow(10,n);
     }
 
-    // PAPI 
-	int EventSet = PAPI_NULL;
+    ll primes_size = (n>>6)+1; 
+    unsigned int *primes = new unsigned int[primes_size]();
   	long long values[2];
   	int ret;
-
+    
 	ret = PAPI_library_init( PAPI_VER_CURRENT );
 	if ( ret != PAPI_VER_CURRENT )
 		cout << "FAIL" << endl;
@@ -45,47 +47,46 @@ int main (int argc, char *argv[])
 		cout << "ERRO: Start PAPI" << endl;
     // PAPI
 
-	struct timespec start, finish;
-	double elapsed;
-	if (rank == root) {
-		clock_gettime(CLOCK_MONOTONIC, &start);
-	}
-    
     bool *primes = new bool[n>>1]();
     
     long long k = 3;
 
-    clock_t start, finish;
-    start = clock();
+    struct timespec start, end;
+    clock_gettime(CLOCK_REALTIME, &start);
 
     do
     {
-        for (long long j = k*k ; j<n ; j+=k<<1)
-        {   primes[j>>1]=true;
+        for (long long j = k*k ; j<n ; j+=k<<1) {   
+            primes[j>>6]|=(1<<((j>>1)&31));
         }
         
         do
         {
             k+=2;
-        }while (k*k <= n && primes[k>>1]);
+        }while (k*k <= n && primes[k>>6]&(1<<((k>>1)&31)));
         
     } while (k*k <= n);
     
     unsigned long long count = 0;
-    for (int i=1; i<n; i+=2)
-        if (!primes[i>>1])
-            //cout << i << " ";
-            ++count;
+    for (int i=0; i<primes_size; i++) {
+        count += 32 - __builtin_popcount(primes[i]);
+//        for(int j=1; j<=32;j++) {
+//            if(((i*32+j)*2+1) >= n) break;
+//            if(!(primes[i]&(1<<j))) cout << i << '\t' << j << '\t' << ((i*32+j)*2+1) << endl;
+//        }
+    }
+    count -= ((primes_size<<6) - n)>>1;
+    
+    clock_gettime(CLOCK_REALTIME, &end);
 
-    finish = clock();
-    double cpu_time_used = (double(finish-start)/CLOCKS_PER_SEC);
+    double time_spent = (end.tv_sec - start.tv_sec) +
+                        (end.tv_nsec - start.tv_nsec) / 1000000000.0;
 
-    printf("Loop took %f seconds to execute. Found %llu primes\n", cpu_time_used, count);
+    printf("Loop took %f seconds to execute. Found %llu primes\n", time_spent, count);
 
-    // PAPI 
-	ret = PAPI_stop(EventSet, values);
-  		if (ret != PAPI_OK) cout << "ERRO: Stop PAPI" << endl;
-
+    ret = PAPI_stop(EventSet, values);
+    if (ret != PAPI_OK) cout << "ERRO: Stop PAPI" << endl;
+        
     printf("\tL1 DCM: %lld \n", values[0]);
     printf("\tL2 DCM: %lld \n", values[1]);
 
